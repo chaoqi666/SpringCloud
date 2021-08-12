f781d7fd10f0cc8be378c3a6285954945ea2fd6f
qc <qichao151128@gmail.com>
2021-08-12 22:05:41 +0800

    first commit

---
+++ springcloud-api/src/main/java/com/qi/springcloud/service/DeptClientService.java
@@ -0,0 +1,24 @@
+package com.qi.springcloud.service;
+
+import com.qi.springcloud.pojo.Dept;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Component //注册到spring容器中
+//@FeignClient:微服务客户端注解,value:指定微服务的名字,这样就可以使Feign客户端直接找到对应的微服务
+@FeignClient(value = "SPRINGCLOUD-PROVIDER-DEPT",fallbackFactory = DeptClientServiceFallBackFactory.class)//fallbackFactory指定降级配置类
+public interface DeptClientService {
+
+    @GetMapping("/dept/get/{id}")
+    public Dept queryById(@PathVariable("id") Long id);
+
+    @GetMapping("/dept/list")
+    public List<Dept> queryAll();
+
+    @GetMapping("/dept/add")
+    public Boolean addDept(Dept dept);
+}
