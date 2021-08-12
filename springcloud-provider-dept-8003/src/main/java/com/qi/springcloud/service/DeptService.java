65d9ff7cf4e03dedc7cd28f518698c15967053ee
qc <qichao151128@gmail.com>
2021-08-12 22:16:42 +0800

    多个服务提供者

---
+++ springcloud-provider-dept-8003/src/main/java/com/qi/springcloud/service/DeptService.java
@@ -0,0 +1,13 @@
+package com.qi.springcloud.service;
+
+import com.qi.springcloud.pojo.Dept;
+
+import java.util.List;
+
+public interface DeptService {
+    public boolean addDept(Dept dept);
+
+    public Dept queryById(Long id);
+
+    public List<Dept> queryAll();
+}
