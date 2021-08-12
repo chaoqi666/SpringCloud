a51f3045bfd3aad7f0dea6fe791b425261f6c845
qc <qichao151128@gmail.com>
2021-08-12 22:11:03 +0800

    consumer

---
+++ springcloud-consumer-dept-80/src/main/java/com/qi/springcloud/controller/DeptConsumerController.java
@@ -0,0 +1,46 @@
+package com.qi.springcloud.controller;
+
+import com.qi.springcloud.pojo.Dept;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+public class DeptConsumerController {
+
+    /**
+     * 理解：消费者，不应该有service层~
+     * RestTemplate .... 供我们直接调用就可以了！ 注册到Spring中
+     * (地址：url, 实体：Map ,Class<T> responseType)
+     * <p>
+     * 提供多种便捷访问远程http服务的方法，简单的Restful服务模板~
+     */
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    //服务提供方地址前缀
+    // private static final String REST_URL_PREFIX = "http://localhost:8001";
+    private static final String REST_URL_PREFIX = "http://SPRINGCLOUD-PROVIDER-DEPT";
+
+    @RequestMapping("/consumer/dept/add")
+    public boolean add(Dept dept) {
+        // postForObject(服务提供方地址(接口),参数实体,返回类型.class)
+        return restTemplate.postForObject(REST_URL_PREFIX + "/dept/add", dept, Boolean.class);
+    }
+
+    @RequestMapping("/consumer/dept/get/{id}")
+    public Dept get(@PathVariable("id") Long id) {
+        // getForObject(服务提供方地址(接口),返回类型.class)
+        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/get/" + id, Dept.class);
+    }
+
+    @RequestMapping("/consumer/dept/list")
+    public List<Dept> list() {
+        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/list", List.class);
+    }
+}
